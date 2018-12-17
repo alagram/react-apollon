6e0d299fea2d0bb0ee433ffa7bf86a3a4ab2c7c2
Albert Agram <albert.agram@gmail.com>
2018-12-17 08:17:51 -0500

    Configure Apollo client

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './style.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./style.css";
+import App from "./App";
 
+const GITHUB_BASE_URL = "https://api.github.com/graphql";
+
+const httpLink = new HttpLink({
+  uri: GITHUB_BASE_URL,
+  headers: {
+    authorization: `Bearer ${
+      process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+    }`
+  }
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache
+});
+
+ReactDOM.render(<App />, document.getElementById("root"));
