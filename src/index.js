4a1a5f684c2aa6e966ccd13e0afc0dbe34374826
Albert Agram <albert.agram@gmail.com>
2019-01-14 07:51:45 -0500

    Run eslint on src/index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { ApolloProvider } from "react-apollo";
-import { ApolloClient } from "apollo-client";
-import { ApolloLink } from "apollo-link";
-import { HttpLink } from "apollo-link-http";
-import { onError } from "apollo-link-error";
-import { RetryLink } from "apollo-link-retry";
-import { InMemoryCache } from "apollo-cache-inmemory";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { RetryLink } from 'apollo-link-retry';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
-import "./style.css";
-import App from "./App";
+import './style.css';
+import App from './App';
 
-const GITHUB_BASE_URL = "https://api.github.com/graphql";
+const GITHUB_BASE_URL = 'https://api.github.com/graphql';
 
 const httpLink = new HttpLink({
   uri: GITHUB_BASE_URL,
   headers: {
     authorization: `bearer ${
       process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
-    }`
-  }
+    }`,
+  },
 });
 
 const errorLink = onError(({ grapghQLErrors, networkError }) => {
@@ -40,12 +40,12 @@ const cache = new InMemoryCache();
 
 const client = new ApolloClient({
   link,
-  cache
+  cache,
 });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
